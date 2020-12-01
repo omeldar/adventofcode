fa2c0b7583d4077a13b3f39bbc9d80ead753f389
Eldar Omerovic <eldar@bluemail.ch>
2020-12-01 12:01:10 +0100

    Created AoC.Foundation for easier completion of Tasks and finished Day 1

---
+++ AoC.1_ReportRepair/Program.cs
@@ -0,0 +1,100 @@
+﻿using AoC.Foundation;
+using System;
+using System.Collections.Generic;
+
+namespace AoC._1_ReportRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne partOne = new PartOne();
+            partOne.Execute();
+
+            PartTwo partTwo = new PartTwo();
+            partTwo.Execute();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class PartOne : DayPart
+    {
+        public void Execute()
+        {
+            ConsoleInit(1, "ReportRepair", 1);
+            string text = System.IO.File.ReadAllText(@"K:\AoC20\AoC-2020\AoC.1_ReportRepair\data.txt");
+            string[] dataSeperated = text.Split('\n');
+            List<int> dataList = new List<int>();
+            foreach (string value in dataSeperated)
+            {
+                dataList.Add(Convert.ToInt32(value));
+            }
+
+            foreach (int dataEntry1 in dataList)
+            {
+                if (SolutionFound)
+                    break;
+
+                foreach (int dataEntry2 in dataList)
+                {
+                    if (dataEntry1 == dataEntry2 || SolutionFound)
+                        break;
+
+                    if (dataEntry1 + dataEntry2 == 2020)
+                    {
+                        Console.WriteLine("Entry 1: " + dataEntry1);
+                        Console.WriteLine("Entry 2: " + dataEntry2);
+                        WriteSolution("Factor: " + dataEntry1 * dataEntry2);
+                        SolutionFound = true;
+                    }
+                }
+            }
+        }
+    }
+    public class PartTwo : DayPart
+    {
+        public void Execute()
+        {
+            ConsoleInit(1, "ReportRepair", 2);
+            string text = System.IO.File.ReadAllText(@"K:\AoC20\AoC-2020\AoC.1_ReportRepair\data.txt");
+            string[] dataSeperated = text.Split('\n');
+            List<int> dataList = new List<int>();
+            foreach (string value in dataSeperated)
+            {
+                dataList.Add(Convert.ToInt32(value));
+            }
+
+            foreach (int dataEntry1 in dataList)
+            {
+                if (SolutionFound)
+                    break;
+                foreach (int dataEntry2 in dataList)
+                {
+                    if (SolutionFound)
+                        break;
+                    if (dataEntry1 == dataEntry2)
+                        continue;
+
+                    foreach (int dataEntry3 in dataList)
+                    {
+                        if (SolutionFound)
+                            break;
+                        if (dataEntry1 == dataEntry3 || dataEntry2 == dataEntry3)
+                            continue;
+
+
+                        if (dataEntry1 + dataEntry2 + dataEntry3 == 2020)
+                        {
+                            Console.WriteLine("Entry 1: " + dataEntry1);
+                            Console.WriteLine("Entry 2: " + dataEntry2);
+                            Console.WriteLine("Entry 3: " + dataEntry3);
+                            WriteSolution("Factor: " + dataEntry1 * dataEntry2 * dataEntry3);
+                            SolutionFound = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AoC.Foundation/DayPart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Foundation
+{
+    public class DayPart
+    {
+        public bool SolutionFound { get; set; }
+        public void ConsoleInit(int day, string dayTaskName, int partId)
+        {
+            Console.WriteLine("------");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Day" + day + ": " + dayTaskName + " Part " + partId);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void WriteSolution(string solution)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(solution);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}

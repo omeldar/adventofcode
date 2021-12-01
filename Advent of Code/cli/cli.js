457b9be61a941802d346a55e518ac141efd7995b
eldar.omerovic <eldar.omerovic@roche.com>
2021-12-01 16:31:03 +0100

    cli push

---
+++ Advent of Code/cli/cli.js
@@ -0,0 +1,70 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+import { runScript } from './main'
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            '--day': Number,
+            '--part': Number,
+            '-d': '--day',
+            '-p': '--part',
+        },
+        {
+            argv: rawArgs.slice(2)
+        }
+    );
+    return {
+        day: args['--day'] || false,
+        part: args['--part'] || false,
+        year: args._[0]
+    }
+}
+
+async function promptForMissingOptions(options){
+
+    const questions = [];
+    if(!options.year) {
+        questions.push({
+            type: 'list',
+            name: 'year',
+            message: '🎅 Please choose which year you want to use.',
+            choices: ['2021', '2022', '2023']
+        });
+    }
+
+    if(!options.day) {
+        questions.push({
+            type: 'number',
+            name: 'day',
+            message: '🎅 Enter your wished day: ',
+            default: 1,
+        });
+    }
+
+    if(!options.part) {
+        questions.push({
+            type: 'number',
+            name: 'part',
+            message: '🎅 Enter your wished part: ',
+            default: 2,
+        });
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+        ...options,
+        year: options.year || answers.year,
+        day: options.day || answers.day,
+        part: options.part || answers.part
+    };
+}
+
+export async function cli(args) {
+    console.clear();
+    console.log("🎄 Advent of Code CLI 🎄");
+    console.log("🎅 Ho Ho Hooo! Nice to see you. Ready to save christmas again?");
+    let options = parseArgumentsIntoOptions(args);
+    options = await promptForMissingOptions(options);
+    runScript(options);
+}
---
+++ Advent of Code/cli/main.js
@@ -0,0 +1,18 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import ncp from 'ncp';
+import path from 'path';
+import { promisify } from 'util';
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles(options) {
+    return copy(options.templateDirectory, options.targetDirectory, {
+        clobber: false,
+    });
+}
+
+export async function runScript(options) {
+    console.log(options);
+}

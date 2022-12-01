8553ca122f6dfd0e658e1371b3eea98b7b8e4536
Eldar Omerovic <eldar.omerovic@roche.com>
2022-12-01 14:28:33 +0100

    Completed Part One on Day 1

--- Advent of Code 2022/Day1/part_one/src/main.rs
+++ Advent of Code 2022/Day1/part_one/src/main.rs
@@ -2,13 +2,30 @@ use std::fs;
 use std::env;
 
 fn main()  {
-    let filepath = String::from("input/input.txt");
-    let mut filedata = String::new();
+    let filepath: String = String::from("input/input.txt");
+    let mut filedata: String = String::new();
     match read_file(filepath){
         Ok(data) => filedata = data,
         Err(err) => println!("{}", err)
     }
-    println!("{}", filedata);
+    let data_string_vector:Vec<&str> = filedata.lines().collect::<Vec<&str>>();
+    let mut highest_calorie_sum: u32 = u32::MIN;
+    let mut current_calorie_counter_sum: u32 = 0;
+    for n in 0..(data_string_vector.len()-1){
+        if data_string_vector[n] == "" {
+            println!("vectoritem empty: {}", data_string_vector[n]);
+            if current_calorie_counter_sum > highest_calorie_sum {
+                highest_calorie_sum = current_calorie_counter_sum;
+            }
+            current_calorie_counter_sum = 0;
+        }
+        else {
+            println!("vectoritem: {}", data_string_vector[n]);
+            let curr_number:u32 = data_string_vector[n].parse::<u32>().unwrap();
+            current_calorie_counter_sum += curr_number;
+        }
+    }
+    println!("{}", highest_calorie_sum);
 }
 
 fn read_file(filepath: String) -> std::io::Result<String> {

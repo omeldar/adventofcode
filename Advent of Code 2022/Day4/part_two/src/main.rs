957ff26ce6d42f06c9a8f5504948a00e03afbcaa
Eldar Omerovic <eldar.work.edu@gmail.com>
2022-12-04 15:47:46 +0100

    Day 4 Part two working

--- Advent of Code 2022/Day4/part_two/src/main.rs
+++ Advent of Code 2022/Day4/part_two/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let data_string = read_file(String::from("input/input.txt"));
+    let result = data_string
+        .lines()
+        .map(|l: &str| l.split(","))
+        .map(|a| a.map(|s: &str| s.split("-").map(|u: &str| u.parse::<u32>().unwrap()).collect_tuple::<(_, _)>().unwrap()).collect_tuple::<(_, _)>().unwrap())
+        .filter(|t: &((u32, u32), (u32, u32))| (t.0.1 >= t.1.0 && t.0.0 <= t.1.1))
+        .count();
+
+    println!("result: {}", result);
+
+}
+
+fn read_file(filepath: String) -> String{
+    return fs::read_to_string(filepath).expect("Unable to read file");
 }

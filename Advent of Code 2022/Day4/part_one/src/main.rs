97f39722002a39f39ff9aad98c05451c7a651b7a
Eldar Omerovic <eldar.work.edu@gmail.com>
2022-12-04 15:13:37 +0100

    Day 4 Part one working

---
+++ Advent of Code 2022/Day4/part_one/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use itertools::Itertools;
+
+fn main() {
+    // read file
+    // get lines
+    // split on , and -
+    // parse strings to int
+    // calculate if sections fully contain others
+    // count and return them
+
+    let data_string = read_file(String::from("input/input.txt"));
+    let result = data_string
+        .lines()
+        .map(|l: &str| l.split(","))
+        .map(|a| a.map(|s: &str| s.split("-").map(|u: &str| u.parse::<u32>().unwrap())
+            .collect_tuple::<(_, _)>().unwrap()).collect_tuple::<(_, _)>().unwrap())
+        .filter(|t: &((u32, u32), (u32, u32))| (t.0.0.ge(&t.1.0) && t.0.1.le(&t.1.1)) || (t.1.0.ge(&t.0.0) && t.1.1.le(&t.0.1)))
+        .count();
+
+    println!("result: {}", result);
+
+}
+
+fn read_file(filepath: String) -> String{
+    return fs::read_to_string(filepath).expect("Unable to read file");
+}

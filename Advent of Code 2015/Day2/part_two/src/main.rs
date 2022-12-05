0aa1f5857afdef3c66474cbbd69453f15980af79
Eldar Omerovic <eldar.work.edu@gmail.com>
2022-12-05 23:21:42 +0100

    2015 Day 2 part two working

--- Advent of Code 2015/Day2/part_two/src/main.rs
+++ Advent of Code 2015/Day2/part_two/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let data_as_string = read_file(String::from("input/input.txt"));
+    let result:u32 = data_as_string
+        .lines()
+        .map(|line: &str| line.split("x").map(|s: &str| s.parse::<u32>().unwrap()).collect_tuple::<(_, _, _)>().unwrap())
+        .map(|t: (u32,u32,u32)| calculate_ribbon_in_feet(t))
+        .sum();
+    println!("{}", result);
+}
+
+fn calculate_ribbon_in_feet((length, width, height): (u32, u32, u32)) -> u32{
+    let mut sides = [length, width, height].to_vec();
+    sides.sort();
+    return (sides[0] * 2 + sides[1] * 2) + (sides[0] * sides[1] * sides[2]);
+}
+
+fn read_file(filepath: String) -> String{
+    return fs::read_to_string(filepath).expect("Unable to read file");
 }

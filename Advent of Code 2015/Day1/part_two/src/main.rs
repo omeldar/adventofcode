b53a083a36d6b5f69ac04d38f1cd38ab08b48ff5
Eldar Omerovic <eldar.work.edu@gmail.com>
2022-12-05 22:25:15 +0100

    2015 Day 1 part two working

--- Advent of Code 2015/Day1/part_two/src/main.rs
+++ Advent of Code 2015/Day1/part_two/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data_as_string: String = read_file(String::from("input/input.txt"));
+    let mut floor: i16 = 0;
+    let mut result = -1;
+    for (i,char) in data_as_string.chars().enumerate(){
+        if floor == -1 && result == -1{
+            result = i as i16;
+        }
+        if char == '('{
+            floor += 1;
+        }
+        else{
+            floor -= 1;
+        }
+    }
+    println!("{}", result);
+}
+
+fn read_file(filepath: String) -> String{
+    return fs::read_to_string(filepath).expect("Unable to read file");
 }

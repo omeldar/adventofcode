7422fd2d8bf42fe632fc785ebbaa9bf2f4a9294c
omeldar <eldar.work.edu@gmail.com>
2023-11-13 23:09:01 +0100

    Finish aoc runner

---
+++ other/csharp/AoC/UXHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC
+{
+    public static class UXHandler
+    {
+        public static void WriteHeader(string header)
+        {
+            Console.Clear();
+            char[] chars = header.ToCharArray();
+            foreach(char c in chars)
+            {
+                if (c == '_')
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                if (c == '/')
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                if (c == '\\')
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.Write(c);
+
+                Console.ForegroundColor= ConsoleColor.White;
+            }
+            Console.Write('\n');
+        }
+    }
+}

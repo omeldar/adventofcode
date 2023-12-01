b9a20a8d0b869f089ce1eeab4d3a630d9639ebc0
omeldar <eldar.work.edu@gmail.com>
2023-12-01 06:56:48 +0100

    Post embarrassing solution to day 1 2023

---
+++ Advent of Code 2023/csharp/day 01/Part1.cs
@@ -0,0 +1,37 @@
+﻿using AoC.Abstract;
+
+namespace AoC._2023.Day1
+{
+    public class Part1 : IPart
+    {
+        public string Run(string input)
+        {
+            List<int> result = new List<int>();
+            foreach (string line in input.Split('\n'))
+            {
+                List<int> values = new List<int>();
+                foreach(char _ch in line)
+                {
+                    try
+                    {
+                        if (Char.IsNumber(_ch))
+                        {
+                            int value = Convert.ToInt32(_ch.ToString());
+                            values.Add(value);
+                        }
+                    } catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                result.Add(Convert.ToInt32(values[0].ToString() + values[values.Count - 1].ToString()));
+            }
+            int sum = 0;
+            foreach(int value in result)
+            {
+                sum += value;
+            }
+            return sum.ToString();
+        }
+    }
+}
---
+++ Advent of Code 2023/csharp/day 01/README.md
@@ -0,0 +1,3 @@
+# ATTENTION
+
+This might be one of the most embarrassing solutions I've ever written.

966685bdc439804e24473332acefa5a26d1c49eb
omeldar <eldar.work.edu@gmail.com>
2023-12-04 21:11:37 +0100

    Complete day 04

--- Advent of Code 2023/haskell/day04.hs
+++ Advent of Code 2023/haskell/day04.hs
@@ -1,3 +1,43 @@
+import Debug.Trace (trace)
+import Data.List.Split (splitOn)
+import Data.List (intersect)
+import qualified Data.IntMap as M
+
 main = do
-    input <- lines <$> readFile "test.txt"
-    print $ input
+    input <- lines <$> readFile "input.txt"
+    print $ sum $ map (\card -> getCardValue $ tail $ last $ splitOn ":" card) input
+    print $ processCards input
+
+-- PART 1
+getNumbers :: String -> [Int]
+getNumbers part = map (\w -> read w) $ filter (/= "") $ words part
+
+calculateValue :: [Int] -> Int
+calculateValue [] = 0
+calculateValue [x] = 1
+calculateValue (x:xs) = 2 * calculateValue xs
+
+getMatchingNumbers :: String -> [Int] 
+getMatchingNumbers line = 
+    let winningNumbers = getNumbers $ head $ splitOn "|" line
+        cardNumbers = getNumbers $ last $ splitOn "|" line
+        matchedNumbers = intersect winningNumbers cardNumbers
+    in matchedNumbers
+
+getCardValue :: String -> Int
+getCardValue line = calculateValue $ getMatchingNumbers line
+
+-- PART 2
+getTotalCards :: [String] -> Int -> M.IntMap Int -> Int
+getTotalCards [] _ cards = sum $ M.elems cards
+getTotalCards (x:xs) currCardIndex cards = 
+    let amtOfMatches = length $ getMatchingNumbers $ last $ splitOn ": " x
+        amtOfCards = cards M.! currCardIndex
+        adjustedCards = foldl (flip $ M.adjust (+ amtOfCards)) cards [currCardIndex+1..currCardIndex+amtOfMatches]
+    in getTotalCards xs (currCardIndex + 1) adjustedCards
+
+cardMap :: [String] -> M.IntMap Int
+cardMap input = M.fromList $ zip [1..] (replicate (length input) 1)
+
+processCards :: [String] -> Int
+processCards input = getTotalCards input 1 (cardMap input)

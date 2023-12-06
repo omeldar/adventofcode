81498ac3d5ccffad8e7309d8d8f5ff4670b0a4b7
omeldar <eldar.work.edu@gmail.com>
2023-12-07 00:15:44 +0100

    Add first try

--- Advent of Code 2023/haskell/day06.hs
+++ Advent of Code 2023/haskell/day06.hs
@@ -3,15 +3,20 @@ type Race = (Int, Int)
 main = do
     let time = [53,89,76,98]
         distance = [313,1090,1214,1201]
+        ttime = [7,15,30]
+        tdistance = [9,40,200]
         races = zip time distance
+        traces = zip ttime tdistance
         oneRace = (53897698, 313109012141201)
+    print $ product $ map (\race -> calculateSolutions race) traces
 
-    print $ product $ map (\race -> getSolutionAmtForRace race 0 0) races -- PART 1
-    print $ getSolutionAmtForRace oneRace 0 0 -- PART 2
-
-getSolutionAmtForRace :: Race -> Int -> Int -> Int
-getSolutionAmtForRace race charge recN
-    | bHold < (snd race) = getSolutionAmtForRace race (bHold + 1) (if beatsRecord then recN + 1 else recN)
-    | otherwise = recN
-    where 
-        beatsRecord = (bHold * (fst race - bHold) > snd race)
+-- formula described in readme, not working yet
+calculateSolutions :: Race -> Int
+calculateSolutions race = (x1 - x2) `div` (2 * (-1))
+    where
+        t = fst race
+        r = snd race
+        x1 = ceiling $ fromIntegral (-t) + sqrt (fromIntegral delta)
+        x2 = floor $ fromIntegral (-t) - sqrt (fromIntegral delta)
+        delta :: Int
+        delta = fromIntegral (t^2+4*r)

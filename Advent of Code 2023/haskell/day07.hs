b9278d54436005c1aac207a7c31c628fabfb3bb7
omeldar <eldar.work.edu@gmail.com>
2023-12-07 21:39:59 +0100

    Complete day 7 part 1

--- Advent of Code 2023/haskell/day07.hs
+++ Advent of Code 2023/haskell/day07.hs
@@ -1,41 +1,40 @@
 import Data.Char (isDigit, digitToInt)
-import Data.List (sortBy)
+import Data.List (sort, sortBy)
 import Data.Map (fromListWith, toList)
 import Debug.Trace (trace)
 
--- ([3,2,10,3,13], 765), ([Cards], BidValue)
-type Hand = ([Int], Int)
+type Hand = ([Int], Int, Int)
 
 main = do
-    input <- lines <$> readFile "test.txt"
+    input <- lines <$> readFile "input.txt"
     let hands = map mapStrHToH input
-    print $ totalWinnings hands
+    print $ sum $ zipWith (*) (map (\(_, bid, _) -> bid) (sortByTypeAndCardVal hands)) [1..]   -- PART 1
 
 -- PART 1
-totalWinnings :: [Hand] -> Int
-totalWinnings hands = 0 -- use sorted list and calculate here
+evalHandType :: [Int] -> Int
+evalHandType hand = case sort $ map (\(_, s) -> s) $ frequency hand of
+        [5] -> 6   -- five of a kind
+        [1, 4] -> 5  -- four of a kind
+        [2, 3] -> 4    -- full house
+        [1, 1, 3] -> 3 -- three of a kind
+        [1, 2, 2] -> 2 -- two pair
+        [1, 1, 1, 2] -> 1  -- one pair
+        _ -> 0    -- nothing
+
+sortByTypeAndCardVal :: [Hand] -> [Hand]
+sortByTypeAndCardVal hands  = sortBy compareHighestCard hands
+
+compareHighestCard :: Hand -> Hand -> Ordering
+compareHighestCard (h1, _, t1) (h2, _, t2)
+    | typeOrd == EQ = compare h1 h2
+    | otherwise = typeOrd
+    where typeOrd = compare t1 t2
 
 -- PART 2
 
 -- HELPER FUNCTIONS
 frequency :: (Ord a) => [a] -> [(a, Int)]
 frequency xs = toList (fromListWith (+) [(x, 1) | x <- xs])
-
-sortByFrequency :: Ord b => [(a, b)] -> [(a, b)]
-sortByFrequency = sortBy (\(_,count1) (_,count2) -> compare count2 count1)
-
-evalHandType :: [Int] -> Int
-evalHandType hand = case map snd $ sortByFrequency $ frequency hand of
-        [5] -> 6   -- five of a kind
-        [4, 1] -> 5  -- four of a kind
-        [3, 2] -> 4    -- full house
-        [3, 1, 1] -> 3 -- three of a kind
-        [2, 2, 1] -> 2 -- two pair
-        [2, 1, 1, 1] -> 1  -- one pair
-        _ -> 0    -- nothing
-
-sortByTypeAndCardVal :: [Hand] -> [Hand]
-sortByTypeAndCardVal hands = hands -- sort here
 
 -- PARSING
 mapCtoValue :: Char -> Int
@@ -47,6 +46,6 @@ mapCtoValue c = case c of
     'T' -> 10
 
 mapStrHToH :: String -> Hand
-mapStrHToH handString = (cards, read $ last $ words handString)
+mapStrHToH handString = (cards, read $ last $ words handString, evalHandType cards)
     where
         cards = map (\c -> if isDigit c then digitToInt c else mapCtoValue c) $ head $ words handString

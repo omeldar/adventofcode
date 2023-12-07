5829f89d3dd6e25c37bac231aa5287b1274e30bb
omeldar <eldar.work.edu@gmail.com>
2023-12-07 01:50:17 +0100

    Refactor func names

--- Advent of Code 2023/haskell/day06.hs
+++ Advent of Code 2023/haskell/day06.hs
@@ -1,11 +1,11 @@
 main = do
     let races = zip [53,89,76,98] [313,1090,1214,1201]
-        oneRace = (53897698, 313109012141201)
-    print $ product $ map (\race -> calculateSolutions race) races
-    print $ calculateSolutions oneRace
+        race = (53897698, 313109012141201)
+    print $ product $ map (\r -> countWays r) races
+    print $ countWays race
 
-calculateSolutions :: (Int, Int) -> Int
-calculateSolutions (t, d) = floor x1 - ceiling x2 + 1
+countWays :: (Int, Int) -> Int
+countWays (t, d) = floor x1 - ceiling x2 + 1
     where
         a = fromIntegral 1
         b = fromIntegral (-t)

a87af963329d7d0e0d5d249a34efbefba9a08e26
omeldar <eldar.work.edu@gmail.com>
2023-12-07 01:43:20 +0100

    Complete improved day 6

--- Advent of Code 2023/haskell/day06.hs
+++ Advent of Code 2023/haskell/day06.hs
@@ -1,24 +1,14 @@
-import Debug.Trace (trace)
-
-type Race = (Int, Int)
-
 main = do
-    let time = [53,89,76,98]
-        distance = [313,1090,1214,1201]
-        ttime = [7,15,30]
-        tdistance = [9,40,200]
-        races = zip time distance
-        traces = zip ttime tdistance
+    let races = zip [53,89,76,98] [313,1090,1214,1201]
         oneRace = (53897698, 313109012141201)
-    print $ product $ map (\race -> calculateSolutions race) traces
+    print $ product $ map (\race -> calculateSolutions race) races
+    print $ calculateSolutions oneRace
 
--- formula described in readme, not working yet
-calculateSolutions :: Race -> Int
-calculateSolutions race = trace (show delta) x1 - x2
+calculateSolutions :: (Int, Int) -> Int
+calculateSolutions (t, d) = floor x1 - ceiling x2 + 1
     where
-        t = fromIntegral $ fst race
-        r = fromIntegral $ snd race
-        x1 = ceiling $ ((-t) + sqrt (delta) / (-2.0))
-        x2 = floor $ ((-t) - sqrt (delta) / (-2.0))
-        delta :: Double
-        delta = (t^2-4*r)
+        a = fromIntegral 1
+        b = fromIntegral (-t)
+        c = fromIntegral (d + 1)
+        x1 = (-b + sqrt (b^2 - 4 * a * c)) / (2 * a)
+        x2 = (-b - sqrt (b^2 - 4 * a * c)) / (2 * a)

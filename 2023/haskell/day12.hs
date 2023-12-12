a4df5eb7e7f167f6637e07b0e6debafdc504c542
Eldar Omerovic <eldar.work.edu@gmail.com>
2023-12-12 16:06:51 +0100

    Add function to split substrings to replace from each other

--- 2023/haskell/day12.hs
+++ 2023/haskell/day12.hs
@@ -1,11 +1,25 @@
 import Data.List.Split (splitOn)
+import Data.List (group)
 
 type Record = (String, [Int])
+type SplitRecord = ([String], [Int])
+
+-- ???.### 1,1,3 - 1 arrangement
+-- .??..??...?##. 1,1,3 - 4 arrangements
+-- ?#?#?#?#?#?#?#? 1,3,1,6 - 1 arrangement
+-- ????.#...#... 4,1,1 - 1 arrangement
+-- ????.######..#####. 1,6,5 - 4 arrangements
+-- ?###???????? 3,2,1 - 10 arrangements
 
 main = do
     input <- lines <$> readFile "test.txt"
     let records = map parse input
-    print records
+    print $ map splitByWorking records
+
+splitByWorking :: Record -> SplitRecord
+splitByWorking record = (splitted, snd record)
+    where
+        splitted = filter ((/= '.') . head) $ group $ fst record
 
 parse :: String -> Record
 parse input = (str, numbers)

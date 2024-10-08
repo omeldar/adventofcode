63facc56a544c0c50372b98520ab72bd4b6cff28
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-10-09 00:17:44 +0200

    trying to read a file

--- 2016/zig/src/day01.zig
+++ 2016/zig/src/day01.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
+const utils = @import("./utils.zig");
 
-pub fn main() void {
-    std.debug.print("Day 1\n", .{});
+pub fn main() !void {
+    var allocator = std.heap.page_allocator;
+
+    const file_content = try utils.readFile(&allocator, "input.txt");
+    defer allocator.free(file_content);
+
+    try std.io.getStdOut().writer().print("File Contents:\n{*}\n", .{file_content});
 }

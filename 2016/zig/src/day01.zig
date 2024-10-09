7ecc734dddff22614f0c9174fb9e5eede724921f
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-10-10 01:54:05 +0200

    Start with logic for day1

--- 2016/zig/src/day01.zig
+++ 2016/zig/src/day01.zig
@@ -1,11 +1,84 @@
 const std = @import("std");
-const utils = @import("./utils.zig");
+
+const Direction = enum { North, East, South, West };
+
+const Position = struct {
+    x: i32,
+    y: i32,
+    facing: Direction,
+
+    pub fn updatePosition(self: *Position, turn: u8, distance: i32) void {
+        switch (turn) {
+            0 => {
+                self.facing = switch (self.facing) {
+                    .North => Direction.West,
+                    .West => Direction.South,
+                    .South => Direction.East,
+                    .East => Direction.North,
+                };
+            },
+            1 => {
+                self.facing = switch (self.facing) {
+                    .North => Direction.East,
+                    .East => Direction.South,
+                    .South => Direction.West,
+                    .West => Direction.North,
+                };
+            },
+            else => {},
+        }
+
+        //Move
+        switch (self.facing) {
+            .North => self.y += distance,
+            .East => self.x += distance,
+            .South => self.y -= distance,
+            .West => self.x -= distance,
+        }
+    }
+};
 
 pub fn main() !void {
-    var allocator = std.heap.page_allocator;
+    const filename = "input.txt";
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
 
-    const file_content = try utils.readFile(&allocator, "input.txt");
-    defer allocator.free(file_content);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    try std.io.getStdOut().writer().print("File Contents:\n{*}\n", .{file_content});
+    const read_buf = try file.readToEndAlloc(allocator, 1024 * 1024);
+    defer allocator.free(read_buf);
+
+    var it = std.mem.split(u8, read_buf, ", ");
+
+    var position = Position{
+        .x = 0,
+        .y = 0,
+        .facing = Direction.North,
+    };
+
+    while (it.next()) |instruction| {
+        const direction = instruction[0];
+
+        const distance = std.fmt.parseInt(i32, instruction[1..], 10) catch {
+            std.debug.print("Failed to parse distance\n", .{});
+            return;
+        };
+
+        var turn: u8 = 0;  // Mutable variable to store turn value
+        switch (direction) {
+            'R' => turn = 1,
+            'L' => turn = 0,
+            else => {
+                std.debug.print("Invalid direction {}\n", .{direction});
+                continue;
+            },
+        };
+
+        position.updatePosition(turn, distance);
+    }
+
+    std.debug.print("Final Position: ({}, {})\n", .{ position.x, position.y });
+    std.debug.print("You are {} blocks away from the target", .{position.x + position.y});
 }

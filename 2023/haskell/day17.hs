5bcb09e5ec2329ff5c1a4ffa51d08d850c397a9c
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-10-13 23:14:39 +0200

    Improve grid creation

--- 2023/haskell/day17.hs
+++ 2023/haskell/day17.hs
@@ -1,19 +1,13 @@
 import Debug.Trace ( trace )
-import Data.Array.Unboxed
+import qualified Data.Array.Unboxed as UA
 
 type Coord = (Int, Int)
-type Grid = UArray Coord Int
+type Grid = UA.UArray Coord Int
 
 data Direction = North | West | South | East deriving (Show, Eq, Enum)
 
 main = do
     input <- lines <$> readFile "test.txt"
-    let grid = gridMap input (length input) (length (head input))
+    let grid :: Grid
+        grid = UA.listArray ((0,0), (length input - 1, length (head input) - 1)) $ map (read . (:[])) $ concat input
     print $ show grid
-
-
-gridMap :: [[Char]] -> Int -> Int -> Grid
-gridMap input numRows numCols = array bounds hlValues
-    where
-        bounds = ((0,0), (numRows - 1, numCols - 1))
-        hlValues = [((x, y), read [char] :: Int) | (y, line) <- zip [0..] input, (x, char) <- zip [0..] line]

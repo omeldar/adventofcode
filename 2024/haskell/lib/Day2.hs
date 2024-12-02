1a6319d631703eebe14d1ac57260440efa042c9f
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-02 05:52:17 +0000

    Improve solution

--- 2024/haskell/lib/Day2.hs
+++ 2024/haskell/lib/Day2.hs
@@ -10,34 +10,28 @@ run filePath = do
     print $ part2 lists
 
 part1 :: [[Int]] -> Int
-part1 lists = length (filter isValidList lists)
+part1 lists = length $ filter isValidList lists
 
 part2 :: [[Int]] -> Int
-part2 lists = length (filter isValidWithRemoval lists)
+part2 lists = length $ filter isValidWithRemoval lists
 
 -- non-exposed
 
 isValidWithRemoval :: [Int] -> Bool
-isValidWithRemoval xs = isValidList xs || canBeMadeValid xs
+isValidWithRemoval xs = isValidList xs || any isValidList (removeOne xs)
 
 removeOne :: [a] -> [[a]]
 removeOne [] = [[]]
 removeOne (x:xs) = xs : map (x :) (removeOne xs)
 
-canBeMadeValid :: [Int] -> Bool
-canBeMadeValid xs = any isValidList (removeOne xs)
-
-isIncreasing :: [Int] -> Bool
-isIncreasing (x:y:ys) = y > x && isIncreasing (y:ys)
-isIncreasing _ = True
-
-isDecreasing :: [Int] -> Bool
-isDecreasing (x:y:ys) = y < x && isDecreasing (y:ys)
-isDecreasing _ = True
+isMonotonic :: (Int -> Int -> Bool) -> [Int] -> Bool
+isMonotonic _ [] = True
+isMonotonic _ [_] = True
+isMonotonic cmp (x:y:ys) = cmp y x && isMonotonic cmp (y:ys)
 
 validStep :: [Int] -> Bool
 validStep (x:y:ys) = abs (y - x) >= 1 && abs (y - x) <= 3 && validStep (y:ys)
 validStep _ = True
 
 isValidList :: [Int] -> Bool
-isValidList xs = (isIncreasing xs || isDecreasing xs) && validStep xs
+isValidList xs = (isMonotonic (>) xs || isMonotonic (<) xs) && validStep xs

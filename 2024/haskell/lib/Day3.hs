09b3103bbe89b7712fa78f342d81eb1f150fd600
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-03 19:24:27 +0100

    Move parts into own functions

--- 2024/haskell/lib/Day3.hs
+++ 2024/haskell/lib/Day3.hs
@@ -9,13 +9,17 @@ expression = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
 run :: String -> IO ()
 run filePath = do
     content <- readFile filePath
-    print $ sum $ map (\[_, x, y] -> read x * read y) $ content =~ expression -- part 1
-    print $ sum $ map (\[_, x, y] -> read x * read y) $ (onlyDo's content) =~ expression -- part 2
+    print $ part1 content
+    print $ part2 content
+
+part1 :: String -> Int
+part1 content = sum $ map (\[_, x, y] -> read x * read y) $ content =~ expression
+
+part2 :: String -> Int
+part2 content = sum $ map (\[_, x, y] -> read x * read y) $ (onlyDo's content) =~ expression
 
 onlyDo's :: String -> String
 onlyDo's str = 
     let newInp = "do()" ++ str
         parts = splitOn "don't()" newInp
     in concatMap (concat . drop 1 . splitOn "do()") parts
-
-

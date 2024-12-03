12b95f020f456a38ab21dec9012f6847fabe9e60
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-03 10:33:12 +0000

    Try part 2

--- 2024/haskell/lib/Day3.hs
+++ 2024/haskell/lib/Day3.hs
@@ -1,15 +1,24 @@
 module Day3 (run) where
 
 import Text.Regex.PCRE ((=~))
+import Data.List (unfoldr)
 
 expression :: String
 expression = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
 
-run :: String -> IO()
+run :: String -> IO ()
 run filePath = do
     content <- readFile filePath
-    let muls = content =~ expression :: [[String]]
-    print $ sum $ map parseAndMultiply muls
+    let allMuls = content =~ expression :: [[String]]
+        segments = splitSegments content
+    print $ sum $ map (\[_, x, y] -> read x * read y) allMuls -- part 1
+    print $ segments -- still working on part 2
 
-parseAndMultiply :: [String] -> Int
-parseAndMultiply [_, x, y] = read x * read y
+splitSegments :: String -> [String]
+splitSegments str = initialSegment : map (!! 0) (str =~ regex)
+  where
+    regex = "(do\\(\\)|don't\\(\\)).*?(?=(do\\(\\)|don't\\(\\))|$)"
+    initialSegment = takeWhile (`notElem` "do()") str
+
+--splitOnDelimiters :: String -> String
+--splitOnDelimiters input = head $ splitOneOf ["do()", "don't()"] input

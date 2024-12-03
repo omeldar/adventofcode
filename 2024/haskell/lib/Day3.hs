a0c3cd68f0c53a5e3a0b27304c94b94c12fe4d37
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-03 19:18:28 +0100

    Finish day 3

--- 2024/haskell/lib/Day3.hs
+++ 2024/haskell/lib/Day3.hs
@@ -9,22 +9,13 @@ expression = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
 run :: String -> IO ()
 run filePath = do
     content <- readFile filePath
-    let allMuls = content =~ expression :: [[String]]
-        segments = splitSegments content
-    print $ sum $ map (\[_, x, y] -> read x * read y) allMuls -- part 1
-    print segments
-    print $ sum $ map sumSegment segments
+    print $ sum $ map (\[_, x, y] -> read x * read y) $ content =~ expression -- part 1
+    print $ sum $ map (\[_, x, y] -> read x * read y) $ (onlyDo's content) =~ expression -- part 2
 
-sumSegment :: String -> Int
-sumSegment segment =
-    let allMuls = segment =~ expression :: [[String]]
-    in sum $ map (\[_, x, y] -> read x * read y) allMuls
+onlyDo's :: String -> String
+onlyDo's str = 
+    let newInp = "do()" ++ str
+        parts = splitOn "don't()" newInp
+    in concatMap (concat . drop 1 . splitOn "do()") parts
 
-splitSegments :: String -> [String]
-splitSegments str = splitOnDelimiters str : filter (startsWithDo) (map (!! 0) (str =~ regex))
-  where
-    regex = "(do\\(\\)|don't\\(\\)).*?(?=(do\\(\\)|don't\\(\\))|$)"
-    startsWithDo segment = take 4 segment == "do()"
 
-splitOnDelimiters :: String -> String
-splitOnDelimiters input = head $ splitOn "don't()" $ head $ splitOn "do()" input

d2caa6b1f3dfec6ca0302d891433183427c173d3
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-05 12:54:28 +0100

    Improve code

--- 2024/haskell/lib/Day5.hs
+++ 2024/haskell/lib/Day5.hs
@@ -6,24 +6,21 @@ import Data.List (elemIndex, delete, nub, sortBy)
 import Common (getMiddle)
 import qualified Data.Map as Map
 
--- Type definitions
-type Rule = (Int, Int)
-type Rules = [Rule]
 type RuleMap = Map.Map Int [Int]
 
 run :: String -> IO ()
 run filePath = do
     [rulesS, updatesS] <- map lines . splitOn "\n\n" <$> readFile filePath
-    let rules = parseRules rulesS
+    let ruleMap = parseRules rulesS
         updates = map (map read . splitOn ",") updatesS
         
-    print $ sum $ map getMiddle $ filter (isValidRow rules) updates
-    print $ sum $ map getMiddle $ map (dynamicSort $ buildRuleMap rules) $ filter (not . isValidRow rules) updates
+    print $ sum $ map getMiddle $ filter (isValidRow ruleMap) updates
+    print $ sum $ map (getMiddle . dynamicSort ruleMap) (filter (not . isValidRow ruleMap) updates)
 
-isValidRow :: Rules -> [Int] -> Bool
+isValidRow :: RuleMap -> [Int] -> Bool
 isValidRow rules row = all (\(a, b) -> case elemIndex a row of
                                          Just posA -> maybe True (> posA) (elemIndex b row)
-                                         Nothing -> True) rules
+                                         Nothing -> True) (flattenRules rules)
 
 customCompare :: RuleMap -> Int -> Int -> Ordering
 customCompare rules x y
@@ -31,16 +28,13 @@ customCompare rules x y
   | y `comesBefore` x = GT
   | otherwise         = EQ
   where
-    comesBefore a b = elem b (Map.findWithDefault [] a rules)
+    comesBefore a b = b `elem` (Map.findWithDefault [] a rules)
 
 dynamicSort :: RuleMap -> [Int] -> [Int]
-dynamicSort rules xs = sortBy (customCompare rules) xs
+dynamicSort rules = sortBy (customCompare rules)
 
--- parsing
+parseRules :: [String] -> RuleMap
+parseRules rulesList = foldr (\rule m -> let [key, value] = map read (splitOn "|" rule) in Map.insertWith (++) key [value] m) Map.empty rulesList
 
-parseRules :: [String] -> Rules
-parseRules rulesList = [(read key, read value) | rule <- rulesList, let [key, value] = splitOn "|" rule]
-
-buildRuleMap :: [Rule] -> RuleMap
-buildRuleMap rules =
-  foldr (\(a, b) m -> Map.insertWith (++) a [b] m) Map.empty rules
+flattenRules :: RuleMap -> [(Int, Int)]
+flattenRules ruleMap = [(key, value) | (key, values) <- Map.toList ruleMap, value <- values]

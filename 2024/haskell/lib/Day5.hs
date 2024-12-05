19650c0f53b0fb9956d039755c48dc35aef906a8
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-05 12:40:57 +0100

    Solve day 5 complete

--- 2024/haskell/lib/Day5.hs
+++ 2024/haskell/lib/Day5.hs
@@ -1,26 +1,46 @@
 module Day5 (run) where
 
-import Data.List.Split ( splitOn )
+import Data.List.Split (splitOn)
+import Data.Maybe (fromMaybe)
+import Data.List (elemIndex, delete, nub, sortBy)
+import Common (getMiddle)
 import qualified Data.Map as Map
-import Data.Map ( Map )
-import Common ( getMiddle )
 
-type Rules = Map.Map Int [Int]
+-- Type definitions
+type Rule = (Int, Int)
+type Rules = [Rule]
+type RuleMap = Map.Map Int [Int]
 
 run :: String -> IO ()
 run filePath = do
     [rulesS, updatesS] <- map lines . splitOn "\n\n" <$> readFile filePath
     let rules = parseRules rulesS
         updates = map (map read . splitOn ",") updatesS
+        
     print $ sum $ map getMiddle $ filter (isValidRow rules) updates
+    print $ sum $ map getMiddle $ map (dynamicSort $ buildRuleMap rules) $ filter (not . isValidRow rules) updates
 
-isValidRow :: Map Int [Int] -> [Int] -> Bool
-isValidRow rulesMap row = all (\(key, values) -> 
-    case lookup key positions of
-            Just keyPos -> all (\v-> maybe True (> keyPos) (lookup v positions)) values
-            Nothing -> True
-        ) (Map.toList rulesMap)
-    where positions = zip row [0..]
+isValidRow :: Rules -> [Int] -> Bool
+isValidRow rules row = all (\(a, b) -> case elemIndex a row of
+                                         Just posA -> maybe True (> posA) (elemIndex b row)
+                                         Nothing -> True) rules
 
-parseRules :: [String] -> Map Int [Int]
-parseRules rulesList = Map.fromListWith (++) [(read key, [read value]) | rule <- rulesList, let [key, value] = splitOn "|" rule]
+customCompare :: RuleMap -> Int -> Int -> Ordering
+customCompare rules x y
+  | x `comesBefore` y = LT
+  | y `comesBefore` x = GT
+  | otherwise         = EQ
+  where
+    comesBefore a b = elem b (Map.findWithDefault [] a rules)
+
+dynamicSort :: RuleMap -> [Int] -> [Int]
+dynamicSort rules xs = sortBy (customCompare rules) xs
+
+-- parsing
+
+parseRules :: [String] -> Rules
+parseRules rulesList = [(read key, read value) | rule <- rulesList, let [key, value] = splitOn "|" rule]
+
+buildRuleMap :: [Rule] -> RuleMap
+buildRuleMap rules =
+  foldr (\(a, b) m -> Map.insertWith (++) a [b] m) Map.empty rules

f8863b0a44541014e995c8d3a06fa72f9c9db580
Eldar Omerovic <eldar.work.edu@gmail.com>
2024-12-06 18:30:02 +0100

    Try solve day 2

--- 2024/haskell/lib/Day6.hs
+++ 2024/haskell/lib/Day6.hs
@@ -1,6 +1,6 @@
 module Day6 (run) where
 
-import Data.Array.Unboxed (assocs, bounds, inRange, (!))
+import Data.Array.Unboxed (assocs, bounds, inRange, (!), (//))
 import Data.List (nub)
 import Common (createGrid, Grid)
 
@@ -11,18 +11,40 @@ run :: String -> IO ()
 run filePath = do
     grid <- createGrid <$> readFile filePath
     print $ part1 grid
+    print $ part2 grid  -- ~6 min
 
 part1 :: Grid -> Int
 part1 grid = length $ getVisited grid (startPos '^' grid) (-1, 0) []
 
+part2 :: Grid -> Int
+part2 grid = length $ filter (\g -> isLoop g (startPos '^' g) (-1, 0) []) $ allPossibilities grid
+
 getVisited :: Grid -> Position -> Direction -> [Position] -> [Position]
 getVisited grid (y,x) dir visited
-    | isNextOut = nub ((y,x) : visited)
-    | otherwise = getVisited grid (y + fst newDir, x + snd newDir) newDir ((y,x) : visited)
-    where 
-        isNextOut = not $ inRange (bounds grid) (y + fst dir, x + snd dir)
-        nextChar = grid ! (y + fst dir, x + snd dir)
-        newDir = if nextChar == '#' then turnRight dir else dir
+    | isNextOut grid (y,x) dir = nub ((y,x) : visited)
+    | otherwise = getVisited grid (y + fst newDir', x + snd newDir') newDir' ((y,x) : visited)
+    where newDir' = newDir grid (y, x) dir
+
+isLoop :: Grid -> Position -> Direction -> [(Position, Direction)] -> Bool
+isLoop grid (y,x) dir visited
+    | isNextOut grid (y,x) dir = False
+    | ifCurrVisited = True
+    | otherwise = isLoop grid (y + fst newDir', x + snd newDir') newDir' (((y, x), dir) : visited)
+    where
+        ifCurrVisited = any (\r -> r == ((y,x), dir)) visited
+        newDir' = newDir grid (y, x) dir
+
+allPossibilities :: Grid -> [Grid]
+allPossibilities grid = [grid // [(pos, '#')] | (pos,val) <- assocs grid, val == '.']
+
+newDir :: Grid -> Position -> Direction -> Direction
+newDir grid (y,x) dir = if (nextChar grid (y,x) dir) == '#' then turnRight dir else dir
+
+nextChar :: Grid -> Position -> Direction -> Char
+nextChar grid (y,x) dir = grid ! (y + fst dir, x + snd dir)
+
+isNextOut :: Grid -> Position -> Direction -> Bool
+isNextOut grid (y, x) dir = not $ inRange (bounds grid) (y + fst dir, x + snd dir)
 
 turnRight :: Direction -> Direction
 turnRight (0, 1) = (1, 0)
